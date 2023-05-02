75a8387b6ba25096a618a21aa048c9782e18ae90
M Vikas Reddy <119840539+MallidiVikasR123@users.noreply.github.com>
2023-05-02 21:29:30 +0530

    #include<stdio.h>
    int main(){
        
        int l,b,w,c,co;
        scanf("%d %d %d %d",&l,&b,&w,&c);
        co=(((l+2*w)*(b+2*w))-(l*b))*c;
        printf("%d",co);
        
        return 0;
    }

---
+++ Cost_to_fence_the_park_outside.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+    
+    int l,b,w,c,co;
+    scanf("%d %d %d %d",&l,&b,&w,&c);
+    co=(((l+2*w)*(b+2*w))-(l*b))*c;
+    printf("%d",co);
+    
+    return 0;
+}
