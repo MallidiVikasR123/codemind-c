0524a403d3fd2869eca4492d924967cab54c4e83
M Vikas Reddy <119840539+MallidiVikasR123@users.noreply.github.com>
2023-05-17 13:12:15 +0530

    #include<stdio.h>
    int main(){
        
        int n,i;
        scanf("%d",&n);
        for(i=2;i<=n;i++){
            if(n%i==0) break;
        }
        if(i==n) printf("prime");
        else printf("not a prime");
        
        return 0;
    }

---
+++ prime_number.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+    
+    int n,i;
+    scanf("%d",&n);
+    for(i=2;i<=n;i++){
+        if(n%i==0) break;
+    }
+    if(i==n) printf("prime");
+    else printf("not a prime");
+    
+    return 0;
+}
