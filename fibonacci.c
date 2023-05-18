5f85231121260187bad510f919a8421b2d32419f
M Vikas Reddy <119840539+MallidiVikasR123@users.noreply.github.com>
2023-05-18 19:22:51 +0530

    #include<stdio.h>
    int main(){
        
        int n,i;
        scanf("%d",&n);
        int a[n];
        for(i=0;i<n;i++){
            a[i]=a[i-1]+a[i-2];
            a[0]=0;
            a[1]=1;
            printf("%d ",a[i]);
        }
    
        return 0;
    }

---
+++ fibonacci.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+    
+    int n,i;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++){
+        a[i]=a[i-1]+a[i-2];
+        a[0]=0;
+        a[1]=1;
+        printf("%d ",a[i]);
+    }
+
+    return 0;
+}
