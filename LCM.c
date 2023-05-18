980d42c550235c0274475a57f97fc1d8110370e6
M Vikas Reddy <119840539+MallidiVikasR123@users.noreply.github.com>
2023-05-18 19:08:10 +0530

    #include<stdio.h>
    int main(){
        
        int a,b,max;
        scanf("%d %d",&a,&b);
        max=(a>b)?a:b;
        while(1){
            if(max%a==0 && max%b==0) break;
            max++;
        }
        printf("%d",max);
        
        return 0;
    }

---
+++ LCM.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+    
+    int a,b,max;
+    scanf("%d %d",&a,&b);
+    max=(a>b)?a:b;
+    while(1){
+        if(max%a==0 && max%b==0) break;
+        max++;
+    }
+    printf("%d",max);
+    
+    return 0;
+}
