b61688fe3ab379504739b627112e4d3011b3ac9c
M Vikas Reddy <119840539+MallidiVikasR123@users.noreply.github.com>
2023-05-18 19:18:36 +0530

    #include<stdio.h>
    int main(){
        
        int a,b,max;
        scanf("%d %d",&a,&b);
        max=(a>b)?a:b;
        while(1){
            if(max%a==0 && max%b==0){
                break;
            }
            max++;
        }
        printf("%d",(a*b)/max);
        
        return 0;
    }

--- GCD_or_HCF.c
+++ GCD_or_HCF.c
@@ -5,7 +5,12 @@ int main(){
     int a,b,max;
     scanf("%d %d",&a,&b);
     max=(a>b)?a:b;
-    while(max%a!=0 || max%b!=0) max++;
+    while(1){
+        if(max%a==0 && max%b==0){
+            break;
+        }
+        max++;
+    }
     printf("%d",(a*b)/max);
     
     return 0;
