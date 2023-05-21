c740e01a7d92d14079c057574740f336945f3dfa
M Vikas Reddy <119840539+MallidiVikasR123@users.noreply.github.com>
2023-05-21 10:26:24 +0530

    #include<stdio.h> int palindrome(int x); int prime(int x); int main() {     int n,i,cnt=0,j,x,res;     scanf("%d",&n);     for(i=n+1;i<n*20;i++){         x=palindrome(i);         if(x==1)         {             res=prime(i);             if(res==1)             {                 printf("%d",i);                 break;             }         }     } } int palindrome(int x) {     int y=x,rem,rev=0;     while(y!=0)     {        rem=y%10;        rev=rev*10+rem;        y/=10;     }     if(rev==x)return 1;     else return 0; } int prime(int x) {     int i,cnt=0;     for(i=1;i<=x;i++)     {         if(x%i==0)cnt++;     }     if(cnt==2)return 1;     else return 0; }

--- Program_to_find_out_the_next_prime_palindrome_number.c
+++ Program_to_find_out_the_next_prime_palindrome_number.c
@@ -1,23 +1,42 @@
 #include<stdio.h>
-
-int main(){
-    
-    int n,rev=0,r,t,i;
+int palindrome(int x);
+int prime(int x);
+int main()
+{
+    int n,i,cnt=0,j,x,res;
     scanf("%d",&n);
-    while(1){
-        rev=0;
-        t=++n;
-        while(t){
-            r=t%10;
-            rev=rev*10+r;
-            t/=10;
-        }
-        if(rev==n){
-            for(i=2;i<=n;i++) if(n%i==0) break;
-            if(i==n) break;
+    for(i=n+1;i<n*20;i++){
+        x=palindrome(i);
+        if(x==1)
+        {
+            res=prime(i);
+            if(res==1)
+            {
+                printf("%d",i);
+                break;
+            }
         }
     }
-    printf("%d",n);
-    
-    return 0;
+}
+int palindrome(int x)
+{
+    int y=x,rem,rev=0;
+    while(y!=0)
+    {
+       rem=y%10;
+       rev=rev*10+rem;
+       y/=10;
+    }
+    if(rev==x)return 1;
+    else return 0;
+}
+int prime(int x)
+{
+    int i,cnt=0;
+    for(i=1;i<=x;i++)
+    {
+        if(x%i==0)cnt++;
+    }
+    if(cnt==2)return 1;
+    else return 0;
 }
