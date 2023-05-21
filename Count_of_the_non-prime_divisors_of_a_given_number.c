768778933d6e44de9fab22a04331a0ac09a0eea1
M Vikas Reddy <119840539+MallidiVikasR123@users.noreply.github.com>
2023-05-21 10:25:36 +0530

    #include<stdio.h> int prime(int i); int main() {     int n,i,cnt=1,x;     scanf("%d",&n);     for(i=2;i<=n;i++)     {         if(n%i==0)         {             x=prime(i);             if(x==0)cnt++;         }     }     printf("%d",cnt); } int prime(int x) {     int i,cnt=0;         for(i=1;i<=x;i++)         {             if(x%i==0)cnt++;         }         if(cnt==2)return 1;         else return 0; }

--- Count_of_the_non-prime_divisors_of_a_given_number.c
+++ Count_of_the_non-prime_divisors_of_a_given_number.c
@@ -1,18 +1,26 @@
 #include<stdio.h>
-
-int main(){
-
-    int n,i,j,pd=0,npd,d=0;
+int prime(int i);
+int main()
+{
+    int n,i,cnt=1,x;
     scanf("%d",&n);
-    for(i=1;i<=n;i++){
-        if(n%i==0){
-            d++;
-            for(j=2;j<=i;j++) if(i%j==0) break;
-            if(i==j) pd++;
+    for(i=2;i<=n;i++)
+    {
+        if(n%i==0)
+        {
+            x=prime(i);
+            if(x==0)cnt++;
         }
     }
-    npd=d-pd;
-    printf("%d",npd);
-
-    return 0;
+    printf("%d",cnt);
+}
+int prime(int x)
+{
+    int i,cnt=0;
+        for(i=1;i<=x;i++)
+        {
+            if(x%i==0)cnt++;
+        }
+        if(cnt==2)return 1;
+        else return 0;
 }
