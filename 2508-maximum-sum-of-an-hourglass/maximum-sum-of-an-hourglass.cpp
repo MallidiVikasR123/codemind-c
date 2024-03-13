9c4794d8aab5e8772d4581dbe148673883c2739b
M Vikas Reddy <119840539+MallidiVikasR123@users.noreply.github.com>
2024-03-13 10:20:53 +0530

    Time: 28 ms (36.30%) | Memory: 15.6 MB (73.77%) - LeetSync

---
+++ 2508-maximum-sum-of-an-hourglass/maximum-sum-of-an-hourglass.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxSum(vector<vector<int>>& grid) {
+        int maxx=INT_MIN;
+        for(int i=0;i<grid.size()-2;i++)
+        {
+            for(int j=0;j<grid[0].size()-2;j++)
+            {
+                int s=0;
+                for(int k1=i;k1<3+i and (3+i)<=grid.size();k1++)
+                {
+                    for(int k2=j;k2<3+j and (3+j)<=grid[0].size();k2++)
+                    {
+                        if(k1==i+1 and k2!=j+1) continue;
+                        s+=grid[k1][k2];
+                    }
+                }
+                maxx=max(s,maxx);
+            }
+        }
+        return maxx;
+    }
+};
