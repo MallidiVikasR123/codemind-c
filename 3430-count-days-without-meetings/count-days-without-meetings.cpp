1a02047c0a3dae73349079a7749c399e8ec72ee4
M Vikas Reddy <119840539+MallidiVikasR123@users.noreply.github.com>
2024-06-21 13:46:49 +0530

    Time: 343 ms (33.31%) | Memory: 125.5 MB (69.77%) - LeetSync

--- 3430-count-days-without-meetings/count-days-without-meetings.cpp
+++ 3430-count-days-without-meetings/count-days-without-meetings.cpp
@@ -1,13 +1,8 @@
 class Solution {
 public:
     int countDays(int days, vector<vector<int>>& meetings) {
-        int c=0;
+        int c=0,i=0,ll=0,m=0,mi=0;
         sort(meetings.begin(),meetings.end());
-        for(int i=0;i<meetings.size();i++){
-            cout<<"["<<meetings[i][0]<<","<<meetings[i][1]<<"],";
-        }
-        int i=0;
-        int m=0,ll=0,mi=0;
         while(i<meetings.size()){
             if(i+1<meetings.size() and (meetings[i][1]>=meetings[i+1][0] or m>=meetings[i+1][0])){
                 if(ll==0){
@@ -15,18 +10,13 @@ public:
                     ll=1;
                 }
                 m=max(m,max(meetings[i+1][1],meetings[i][1]));
-                cout<<m<<" ";
             }
             else if(ll!=0)
             {
                 days-=(m-mi)+1;
-                cout<<m<<" "<<mi
-;                ll=0;
+;               ll=0;
             }
-            else
-            {
-                days-=(meetings[i][1]-meetings[i][0]+1);
-            }
+            else days-=(meetings[i][1]-meetings[i][0]+1);
             i++;
         }
         return days;
