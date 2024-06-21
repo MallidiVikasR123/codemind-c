d164cd9d89b6fc96dbd56be4d87d9ccb2eb751ec
M Vikas Reddy <119840539+MallidiVikasR123@users.noreply.github.com>
2024-06-21 10:32:07 +0530

    Time: 24 ms (59.49%) | Memory: 34.4 MB (16.52%) - LeetSync

--- 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.cpp
+++ 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.cpp
@@ -3,7 +3,7 @@ public:
     int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int minutes) {
         std::ios_base::sync_with_stdio(false);
         std::cin.tie(NULL);
-        int c=0,c1=0,m=0;
+        int c=0,c1=0,m=0,j=0;
         for(int i=0;i<customers.size();i++)
         {
             if(grumpy[i]==0)
@@ -11,19 +11,23 @@ public:
                 c+=customers[i];
                 customers[i]=0;
             }
+            if(i<minutes) c1+=customers[i];
+            else{
+                m=max(c1,m);
+                c1-=customers[j];
+                c1+=customers[i];
+                j++;
+            }
         }
-        for(int i=0;i<minutes;i++)
-        {
-            c1+=customers[i];
-        }
-        int j=0;
-        for(int i=minutes;i<customers.size();i++)
-        {
-            m=max(c1,m);
-            c1-=customers[j];
-            c1+=customers[i];
-            j++;
-        }
+        // for(int i=0;i<minutes;i++)
+        // {
+            
+        // }
+        // int j=0;
+        // for(int i=0;i<customers.size();i++)
+        // {
+            
+        // }
         m=max(c1,m);
         return c+m;
     }
