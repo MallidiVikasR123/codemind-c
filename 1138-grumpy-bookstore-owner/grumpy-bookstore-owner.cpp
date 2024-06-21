ca85ef113ca1d9ab3798f0f55a7f35835d4a8b5b
M Vikas Reddy <119840539+MallidiVikasR123@users.noreply.github.com>
2024-06-21 10:32:46 +0530

    Time: 20 ms (80.70%) | Memory: 34.4 MB (47.97%) - LeetSync

--- 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.cpp
+++ 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
     int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int minutes) {
-        std::ios_base::sync_with_stdio(false);
-        std::cin.tie(NULL);
         int c=0,c1=0,m=0,j=0;
         for(int i=0;i<customers.size();i++)
         {
@@ -19,15 +17,6 @@ public:
                 j++;
             }
         }
-        // for(int i=0;i<minutes;i++)
-        // {
-            
-        // }
-        // int j=0;
-        // for(int i=0;i<customers.size();i++)
-        // {
-            
-        // }
         m=max(c1,m);
         return c+m;
     }
