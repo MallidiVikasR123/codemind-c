84520b193f34a0580cabcb22435b245dfaaf7820
M Vikas Reddy <119840539+MallidiVikasR123@users.noreply.github.com>
2024-10-19 11:32:28 +0530

    Time: 207 ms (5.64%) | Memory: 157.6 MB (7.68%) - LeetSync

--- 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.cpp
+++ 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     char findKthBit(int n, int k) {
         string store = "0";
-        for(int i=2;i<=n;i++){
+        for(int i=1;i<=n;i++){
             string temp="";
             for(int j=0;j<store.size();j++) temp+=(store[j]=='0'? '1' :'0');
             reverse(temp.begin(), temp.end());
